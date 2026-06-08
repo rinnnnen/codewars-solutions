995551ffa5300d85926678719cb1580a84bfa32b
rinen <anastasya.prasolova@bk.ru>
2026-06-08 23:00:37 +0300

    twice_as_old.py

--- 8kyu/twice_as_old.py
+++ 8kyu/twice_as_old.py
@@ -2,5 +2,5 @@
 def twice_as_old(dad_years_old, son_years_old):
     x =2*son_years_old-dad_years_old # from dad_years_old - x == 2 * (son_years_old - x )
     if x < 0:
-        return -x
+        return -x # return - the computer received and continues to use
     return x 
