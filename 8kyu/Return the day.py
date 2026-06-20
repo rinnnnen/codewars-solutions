ec5f641c8f642527448bd6e5ce8b47eb1028e41a
rinen <anastasya.prasolova@bk.ru>
2026-06-21 00:47:55 +0300

    Return the day.py

--- 8kyu/Return the day.py
+++ 8kyu/Return the day.py
@@ -1,5 +1,5 @@
 def whatday(num):
-    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"]
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     if 1 <= num <= 7:
         return days[num - 1] #bc counting from zero
     else:
