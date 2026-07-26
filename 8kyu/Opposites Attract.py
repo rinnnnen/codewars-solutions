edada00d4b4328f2464f89e8ebf8cfcf22b2095f
rinen <anastasya.prasolova@bk.ru>
2026-07-26 13:09:00 +0300

    Update Opposites Attract.py

--- 8kyu/Opposites Attract.py
+++ 8kyu/Opposites Attract.py
@@ -1,5 +1,2 @@
 def lovefunc(flower1, flower2):
-    if (flower1 % 2 == 0 and flower2 % 2 == 1) or (flower1 % 2 == 1 and flower2 % 2 == 0):
-        return True
-    else:
-        return False
+    return flower1 % 2 != flower2 % 2
